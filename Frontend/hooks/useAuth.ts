12649e4191dea77617309485c3b6f40dd4da7b40
risnariha <risnariha994563@gmail.com>
2026-02-20 00:02:35 +0530

    hooks use

--- Frontend/hooks/useAuth.ts
+++ Frontend/hooks/useAuth.ts
@@ -1,3 +1,3 @@
 Hooks like ((((useEffect))))) handle side effects (data fetching, subscriptions, DOM manipulation):
 Before hooks, state could only be used in class components. Hooks allow functional components(((useState))) to have state: 
-Custom hooks allow you to extract and reuse logic across components:
+Custom hooks allow you to extract and reuse logic across components:{{useLocalStorage}}
